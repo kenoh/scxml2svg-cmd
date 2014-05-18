fba6d47bc2de85490f59f29ec48d821b4003b9b4
zuzi.marek@gmail.com <zuzi.marek@gmail.com>
2014-05-18 13:06:15 +0200

    Modification of java classes

--- src/backend/Scxml2Svg.java
+++ src/backend/Scxml2Svg.java
@@ -1,8 +1,12 @@
 package cz.muni.fi.pb138.scxml2svg;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 
 /**
@@ -14,34 +18,71 @@ import javax.xml.transform.TransformerException;
  */
 public class Scxml2Svg {
 
+    public static void main(String[] args) {
+        Scxml2Svg program = new Scxml2Svg();
+
+        program.process(args);
+    }
+
     public void process(String[] args) {
+        validateArgs(args);
+        
         String srcFileName = args[0];
         String dstFileName = args[1];
 
         File srcFile = new File(srcFileName);
         File dstFile = new File(dstFileName);
         File dotFile = new File("graph.dot");
-        //File xsdFile = new File("");
-        //File templateFile = new File("");
+        
+        File xsdDir = new File("xsd");
+        File templateFile = new File("scxml-to-dot.xsl");
 
         XmlTransformator trans = new XmlTransformator();
         XmlValidator valid = new XmlValidator();
         GraphVizConverter converter = new GraphVizConverter();
 
         // validate source file
-        //valid.validateAgainstXsd(srcFile, xsdFile);
-        //
+        boolean isValid = false;
+        try {
+            isValid = valid.validateAgainstXsdDir(srcFile, xsdDir);
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found.");
+            System.exit(1);
+        }
+
+        if (!isValid) {
+            System.err.println("Source file is not valid SCXML file. Aborting.");
+            System.exit(1);
+        } else {
+            System.out.println("Source file " + srcFileName + " is valid.");
+        }
+
         // transform to DOT
-        //try {
-        //    trans.transform(srcFile, templateFile, dotFile);
-        //} catch (TransformerException ex) {
-        //    Logger.getLogger(Scxml2Svg.class.getName()).log(Level.SEVERE, null, ex);
-        //}
+        try {
+            trans.transform(srcFile, templateFile, dotFile);
+        } catch (TransformerConfigurationException e) {
+            System.err.println("Problem with template file.");
+            System.exit(1);
+        } catch (TransformerException ex) {
+            System.err.println("Transformation failed.");
+            System.exit(1);
+        }
+        
+        System.out.println("Transformation to DOT complete.");
+        
         // convert DOT to SVG
         converter.convertDotToSvg(dotFile, dstFile);
     }
 
     private void validateArgs(String[] args) {
         // exit the program if arguments are not valid
+        if(args == null) {
+            System.err.println("No arguments given.");
+            System.exit(1);
+        }
+        if(args.length < 2) {
+            System.err.println("Not enough arguments given.");
+            System.exit(1);
+        }
     }
 }
--- src/backend/XmlValidator.java
+++ src/backend/XmlValidator.java
@@ -1,8 +1,14 @@
 package cz.muni.fi.pb138.scxml2svg;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
@@ -16,23 +22,55 @@ import org.xml.sax.SAXException;
  */
 public class XmlValidator {
 
-    public boolean validateAgainstXsd(File xmlFile, File xsdFile) {
+    public boolean validateAgainstXsdDir(File xmlFile, File xsdDir) throws FileNotFoundException {
+        InputStream xml = new FileInputStream(xmlFile);
+        
+        File[] files = xsdDir.listFiles();
+        Source[] sources = new Source[files.length];
+        
+        // load all xsd files in the dir
+        for(int i=0;i<files.length;i++) {
+            File f = files[i];
+            sources[i] = new StreamSource(f);
+        }
+
+        return validateAgainstXsd(xml, sources);
+    }
+    
+    public boolean validateAgainstXsd(File xmlFile, File xsdFile) throws FileNotFoundException {
+        InputStream xml = new FileInputStream(xmlFile);
+        InputStream xsd = new FileInputStream(xsdFile);
+        
+        return validateAgainstXsd(xml, xsd);
+    }
+
+    public boolean validateAgainstXsd(InputStream xmlStream, InputStream xsdStream) {
+        Source[] sources = new Source[1];
+        sources[0] = new StreamSource(xsdStream);
+        
+        return validateAgainstXsd(xmlStream, sources);
+    }
+    
+    public boolean validateAgainstXsd(InputStream xmlStream, Source[] sources) {
         // create schema factory
         SchemaFactory sFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
         // load schema from file
         Schema schema = null;
         try {
-            schema = sFactory.newSchema(xsdFile);
+            schema = sFactory.newSchema(sources);
         } catch (SAXException se) {
-
+            System.err.println("Schema not loaded.");
+            System.err.println(se.getMessage());
+            System.exit(1);
         }
 
         // validate xml file using validator from created schema
         Validator validator = schema.newValidator();
         try {
-            validator.validate(new StreamSource(xmlFile));
+            validator.validate(new StreamSource(xmlStream));
             return true;
         } catch (SAXException se) {
+            System.err.println(se.getMessage());
             return false;
         } catch (IOException ioe) {
             return false;
